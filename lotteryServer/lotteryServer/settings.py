fb0e39b39e08091173cecb3a353fca3778b3f749
tzsombor95 <tzsombor95@gmail.com>
2023-01-31 20:11:32 +0100

    connect html files with backend

--- lotteryServer/lotteryServer/settings.py
+++ lotteryServer/lotteryServer/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'lotteryServer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '../frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
